50d690a2283060b2eadcdae962d36e98b8b2697b
Hideaki Tamai <tamadon3776@gmail.com>
2018-06-14 23:14:38 +0900

    番地取得処理を追加

--- address_splitter.go
+++ address_splitter.go
@@ -25,6 +25,7 @@ func trimExtraString(address string) string {
 	result = trimStringRegexp(result, `「.*?」`)
 	// 特定フレーズの後にある文字を削除
 	result = trimStringRegexp(result, `(◎|※|☆|★|◇|◆|□|■|●|○|~|〜).*`)
+	result = trimStringRegexp(result, ` `)
 
 	return result
 }
@@ -61,6 +62,26 @@ func getCity(inputString string) string {
 	return ""
 }
 
+func getAddress(inputString string) string {
+	// 数字
+	num := `[一二三四五六七八九十百千万]|[0-9]|[０-９]`
+	// 繋ぎ文字1：数字と数字の間(末尾以外)
+	// s_str1 := `(丁目|丁|番地|番|号|-|‐|ー|−|の|東|西|南|北)`
+	s_str1 := `(丁目|丁|番地|番|号|-|‐|ー|−|の)`
+	// 繋ぎ文字2：数字と数字の間(末尾)
+	s_str2 := `(丁目|丁|番地|番|号)`
+	// 全ての数字
+	all_num := `(\\d+|` + num + `+)`
+
+	pattern := all_num + `*(` + all_num + `|` + s_str1 + `{1,2})*(` + all_num + `|` + s_str2 + `{1,2})`
+	rep := regexp.MustCompile(pattern)
+	if rep.MatchString(inputString) {
+		return rep.FindAllStringSubmatch(inputString , -1)[0][0]
+	}
+
+	return ""
+}
+
 // 文字列を1行入力
 func StrStdin() (stringInput string) {
     scanner := bufio.NewScanner(os.Stdin)
@@ -73,17 +94,24 @@ func StrStdin() (stringInput string) {
 }
 
 func main() {
-	address := StrStdin()
-	trimedAddress := trimExtraString(address)
-	prefecture := getPrefecture(trimedAddress)
+	inputStr := StrStdin()
+	trimedStr := trimExtraString(inputStr)
+	prefecture := getPrefecture(trimedStr)
 
-	trimedPrefectureAddress := trimStringRegexp(trimedAddress, prefecture)
-	city := getCity(trimedPrefectureAddress)
+	trimedPrefectureStr := trimStringRegexp(trimedStr, prefecture)
+	city := getCity(trimedPrefectureStr)
 
-	etc := trimStringRegexp(trimedPrefectureAddress, city)
+	trimedPrefectureCityStr := trimStringRegexp(trimedPrefectureStr, city)
+	address := getAddress(trimedPrefectureCityStr)
 
-	fmt.Println(trimedAddress)
+	town := trimStringRegexp(trimedPrefectureCityStr, address)
+
+	etc := trimStringRegexp(trimedPrefectureStr, city)
+
+	fmt.Println(trimedStr)
 	fmt.Println(prefecture)
 	fmt.Println(city)
+	fmt.Println(town)
+	fmt.Println(address)
 	fmt.Println(etc)
 }
